a1f45e9bbb83abbce16601cfd2c66b58cfc8cea4
Asas <adi10ain@student.lu.se>
2013-11-17 01:49:56 +0100

    Added Abstract class Preference and some subclasses Added iconic buttons of the preferences Cleaned up some code

--- aProfile/src/com/LTH/aprofile/MainActivity.java
+++ aProfile/src/com/LTH/aprofile/MainActivity.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+
+import com.LTH.aprofile.Preferences.BrightnessPreference;
+import com.LTH.aprofile.Preferences.Preference;
+import com.LTH.aprofile.Preferences.SoundLevelPreference;
+
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.wifi.ScanResult;
@@ -11,19 +17,21 @@ import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
-	//CONSTANTS
+	// CONSTANTS
 	private static final int REQUEST_CODE_NEW_PROFILE = 1;
-	
+
 	public static final int APPROVE_NEW_PROFILE = 1;
 	public static final int DECLINE_NEW_PROFILE = 2;
-	
-	
+
 	WifiManager wifi;
 	ListView lv;
 	Button buttonScan;
@@ -34,18 +42,15 @@ public class MainActivity extends Activity implements OnClickListener {
 	ArrayList<HashMap<String, String>> arraylist = new ArrayList<HashMap<String, String>>();
 	SimpleAdapter adapter;
 
-	
 	private TextView TW_currentProfile;
-	private TextView TW_desiredPref;
-	
+
 	private Settings settings;
-	
-	//experimental
+
+	// experimental
 	private Profile currentProfile;
-	private int[] desiredPref = new int[2]; 
-	
+
 	public static Profile targetProfile;
-	
+	private ArrayList<Integer> desiredPref = new ArrayList<Integer>();
 
 	/* Called when the activity is first created. */
 	@Override
@@ -53,97 +58,101 @@ public class MainActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//get UI elements
+		// get UI elements
 		buttonScan = (Button) findViewById(R.id.button_scan);
 		buttonScan.setOnClickListener(this);
 		lv = (ListView) findViewById(R.id.listView);
 		TW_currentProfile = (TextView) findViewById(R.id.currentProfile);
-		TW_desiredPref = (TextView) findViewById(R.id.desiredPreferences);
 
-		//adapter for making list
+		// adapter for making the WiFi list
 		this.adapter = new SimpleAdapter(MainActivity.this, arraylist,
-		android.R.layout.simple_list_item_1, new String[] { ITEM_KEY },
-		new int[] { android.R.id.text1 });
+				android.R.layout.simple_list_item_1, new String[] { ITEM_KEY },
+				new int[] { android.R.id.text1 });
 		lv.setAdapter(this.adapter);
 		
-		//generate fake profiles;
-		Profile p1 = new Profile("NETGEAR","00:13:49:A8:77:4F",10,20, this);
-		Profile p2 = new Profile("EDUROAM","00:11:22:A8:66:9B",0,0, this);
-		Profile p3 = new Profile("Mom use this one","22:31:22:B2:12:46",100,100, this);
-
 		settings = new Settings();
-		
-		settings.addProfile(p1);
-		settings.addProfile(p2);
-		settings.addProfile(p3);
-		
+		loadSettings();
+
 		currentProfile = new Profile();
-		
+
 		showProfiles();
 	}
 
-	
-	//temporary button, simulates a new connection to WiFi AP
-	public void onClick(View view)
-	{
-		
+	// temporary button, simulates a new connection to WiFi AP
+	public void onClick(View view) {
+
 		targetProfile = settings.getProfile("00:11:22:A8:66:9B");
 
 		Intent myIntent = new Intent(this, NewprofileActivity.class);
 		this.startActivityForResult(myIntent, REQUEST_CODE_NEW_PROFILE);
-		
+
 	}
-	
-		
-		
-	// Shows the current profile, its desired preferences and a list of all pre-set profiles 
+
+	// Shows the current profile, its desired preferences and a list of all
+	// pre-set profiles
 	private void showProfiles() {
-		
-		//Displays current active profile
-		TW_currentProfile.setText(""+currentProfile);
-		
-		//Displays current preferences of active profile
-		String prefRow = "";
-		for (int i = 0; i < desiredPref.length; i++)
-			prefRow += desiredPref[i] + " ";
-		
-		TW_desiredPref.setText(prefRow);
-		
-		
-		//Clear old list
+
+		// Displays current active profile
+		TW_currentProfile.setText("" + currentProfile);
+
+		// Displays current preferences of active profile
+		LinearLayout linearLayout = (LinearLayout) findViewById(R.id.desiredPreferences);
+		linearLayout.removeAllViews();
+		linearLayout.addView(currentProfile.genPrefButtons(this, desiredPref));
+
+		// Clear old wifi-profiles list
 		arraylist.clear();
-		
-		
-		//Displays all profiles in a list
+
+		// Displays all profiles in the wifi-profiles list
 		Iterator<Profile> profiles = settings.getProfiles();
-		while(profiles.hasNext()) {
+		while (profiles.hasNext()) {
 			Profile profile = profiles.next();
 			HashMap<String, String> item = new HashMap<String, String>();
-			item.put(ITEM_KEY, ""+profile);
+			item.put(ITEM_KEY, "" + profile);
 			arraylist.add(item);
 			adapter.notifyDataSetChanged();
-			
+
 		}
 	}
-	
-	
-	//Method when returned from called activity
+
+	// Method called when returning from called activity
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-	   
-	    if (requestCode == REQUEST_CODE_NEW_PROFILE) {
-	    	switch (resultCode) {
-	    	//if new profile was approved
-	    	case APPROVE_NEW_PROFILE: 
-	    		currentProfile = targetProfile;
-	    		desiredPref = data.getIntArrayExtra("PREFERENCES");
-	    		currentProfile.loadPref(desiredPref);
-	    		showProfiles();
-	    		break;
-	    	
-	    	}
-	    }
+
+		if (requestCode == REQUEST_CODE_NEW_PROFILE) {
+			switch (resultCode) {
+			// if new profile was approved
+			case APPROVE_NEW_PROFILE:
+				currentProfile = targetProfile;
+				desiredPref = data.getIntegerArrayListExtra("PREFERENCES");
+				currentProfile.loadPref(desiredPref);
+				showProfiles();
+				break;
+
+			}
+		}
 	}
 
+	private void loadSettings() {
+		
+		
+		// generate fake profiles;
+		Profile p1 = new Profile("NETGEAR", "00:13:49:A8:77:4F");
+		SoundLevelPreference pref1 = new SoundLevelPreference(0, this);
+		p1.addPref(pref1);
 
+		Profile p2 = new Profile("EDUROAM", "00:11:22:A8:66:9B");
+		SoundLevelPreference pref2 = new SoundLevelPreference(0, this);
+		BrightnessPreference pref3 = new BrightnessPreference(80, this);
+		p2.addPref(pref2);
+		p2.addPref(pref3);
+
+		Profile p3 = new Profile("Mom use this one", "22:31:22:B2:12:46");
+		
+		
+
+		settings.addProfile(p1);
+		settings.addProfile(p2);
+		settings.addProfile(p3);
+	}
 
 }
--- aProfile/src/com/LTH/aprofile/Profile.java
+++ aProfile/src/com/LTH/aprofile/Profile.java
@@ -1,99 +1,106 @@
 package com.LTH.aprofile;
 
-import android.content.Context;
-import android.media.AudioManager;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.LTH.aprofile.Preferences.Preference;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Profile {
 	// WiFi identifiers
-	private String ESSID; //ex. "NETGEAR"
-	private String BSSID; //ex. "00:11:22:33:44:55"
-	
-	//CONSTANTS
+	private String ESSID; // ex. "NETGEAR"
+	private String BSSID; // ex. "00:11:22:33:44:55"
+
+	// CONSTANTS
 	public static final int SOUNDLEVEL = 0;
 	public static final int BRIGHTNESS = 1;
-		
+	
+	public static final int ALPHA_BTN = 80; // Opacity for toggled off button (0-255)
+	public static final int ALPHA_MAX = 255;
+
 	// preferences
-	private int preferences[] = new int[2];
-	
-	//managers
-	private Context callingContext;
-	private AudioManager audioManager;
-	
+	HashMap<Integer, Preference> preferences = new HashMap<Integer, Preference>();
+
 	public Profile() {
 		this.BSSID = "Not set";
 		this.ESSID = "Profile not set";
 	}
-	
-	public Profile(String ESSID, String BSSID, int soundLevel, int brightness, Context callingContext) {
+
+	public Profile(String ESSID, String BSSID) {
 		this.ESSID = ESSID;
 		this.BSSID = BSSID;
-		preferences[SOUNDLEVEL] = soundLevel;
-		preferences[BRIGHTNESS] = brightness;
-		
-		this.callingContext = callingContext;		
-		audioManager = (AudioManager)callingContext.getSystemService(Context.AUDIO_SERVICE);
 
-		
-		
 	}
-	
-	//loads all preferences
-	public void loadPref() {
-		int[] array = new int[preferences.length];
-		
-		for (int i = 0; i < preferences.length; i++)
-			array[i] = 1;
-		
-		loadPref(array);
-		
+
+	public void addPref(Preference pref) {
+		preferences.put(pref.getType(), pref);
 	}
-	
-	/*loads desired preferences only
-	 * desiredPref, preferences which we want to activate should be represented by value 1
-	 * "preferences[1] = 1" means we want to activate the preferences for BRIGHTNESS
-	 * */
-	public void loadPref(int[] desiredPref) {
-		for (int i = 0; i < desiredPref.length; i++) {
-			if (desiredPref[i] == 1)
-				loadSpecPref(i);
+
+	/*
+	 * loads desired preferences only desiredPref is an ArrayList of the
+	 * integers, IDs, of the preferences which we want to activate
+	 */
+	public void loadPref(ArrayList<Integer> desiredPref) {
+		for (int pref : desiredPref) {
+			Preference p = preferences.get(pref);
+			if (p != null)
+				p.load();
 		}
-		
+
 	}
-	
-	//loads a specific preference
-	private void loadSpecPref(int identifier) {
-		switch (identifier) {
-			case SOUNDLEVEL: System.out.print("SOUND - target "+preferences[SOUNDLEVEL]);
-			setSound(preferences[SOUNDLEVEL]);
-			break;
+
+	// returns a LinearLayout with icons of the profile's preferences
+	public LinearLayout genPrefButtons(Activity activity) {
+		return genPrefButtons(activity, null);
+	}
+
+	// returns a LinearLayout with icons of the profile's preferences with (on/off) visualization
+	public LinearLayout genPrefButtons(Activity activity,
+			ArrayList<Integer> desiredPref) {
+		LinearLayout A = new LinearLayout(activity);
+		A.setOrientation(LinearLayout.HORIZONTAL);
+		for (Preference p : getPref().values()) {
+			final ImageView imageView = new ImageView(activity);
+			imageView.setImageResource(p.getIconResId());
+
+			int alpha = ALPHA_BTN;
+			if (desiredPref == null || desiredPref.contains(p.getType()))
+				alpha = ALPHA_MAX;
+
+			imageView.setAlpha(alpha);
 			
-			case BRIGHTNESS: System.out.print("BRIGHTNESS - target "+preferences[BRIGHTNESS]);
-			break;
-		
-		
+			imageView.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View view) {
+	               	int alpha = (imageView.getImageAlpha() == ALPHA_MAX) ? ALPHA_BTN : ALPHA_MAX;
+	               	imageView.setAlpha(alpha);
+	            }
+	        });
+			imageView.setId(p.getType());
+			A.addView(imageView);
+
 		}
-		
+
+		return A;
 	}
-	
-	//sets ring sound
-	private void setSound(int level) {
-		audioManager.setStreamVolume(AudioManager.STREAM_RING, level, AudioManager.FLAG_ALLOW_RINGER_MODES|AudioManager.FLAG_PLAY_SOUND);
-	}
-	
-	/*compares the argument with the BSSID of the profile
-	Returns true if they are equal
-	*/
+
+	/*
+	 * compares the argument with the BSSID of the profile Returns true if they
+	 * are equal
+	 */
 	public boolean compareBSSID(String BSSID) {
 		return (BSSID.equals(this.BSSID));
 	}
-	
+
 	public String toString() {
 		return ESSID;
 	}
-	
-	public int[] getPref() {
+
+	public HashMap<Integer, Preference> getPref() {
 		return preferences;
 	}
-	
 
 }
