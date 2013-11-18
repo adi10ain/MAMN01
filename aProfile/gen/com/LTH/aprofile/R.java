bde952a25026644289122339a666f734ac0321c1
Jimmy Tieu <ada09jti@student.lu.se>
2013-11-18 18:27:25 +0100

    Added a new class, Comfirm Added some images, arrows. Added a layout, activity_confirm

--- aProfile/gen/com/LTH/aprofile/R.java
+++ aProfile/gen/com/LTH/aprofile/R.java
@@ -21,23 +21,31 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int brightness64=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int volume64=0x7f020002;
+        public static final int arrow_down=0x7f020000;
+        public static final int arrow_left=0x7f020001;
+        public static final int arrow_right=0x7f020002;
+        public static final int arrow_up=0x7f020003;
+        public static final int brightness64=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int volume64=0x7f020006;
     }
     public static final class id {
-        public static final int ProfileName=0x7f080005;
-        public static final int acceptProfile=0x7f080008;
-        public static final int action_settings=0x7f08000b;
-        public static final int button_scan=0x7f080004;
-        public static final int currentProfile=0x7f080001;
-        public static final int declineProfile=0x7f080007;
-        public static final int desiredPreferences=0x7f080002;
-        public static final int linearLayout1=0x7f080000;
-        public static final int listView=0x7f080003;
-        public static final int profileType=0x7f080006;
-        public static final int textView1=0x7f080009;
-        public static final int textView2=0x7f08000a;
+        public static final int ProfileName=0x7f080009;
+        public static final int acceptProfile=0x7f08000c;
+        public static final int action_settings=0x7f08000f;
+        public static final int button_scan=0x7f080008;
+        public static final int currentProfile=0x7f080005;
+        public static final int declineProfile=0x7f08000b;
+        public static final int desiredPreferences=0x7f080006;
+        public static final int image=0x7f080003;
+        public static final int linearLayout1=0x7f080004;
+        public static final int listView=0x7f080007;
+        public static final int profileType=0x7f08000a;
+        public static final int textView1=0x7f08000d;
+        public static final int textView2=0x7f08000e;
+        public static final int textview1=0x7f080000;
+        public static final int textview2=0x7f080001;
+        public static final int textview3=0x7f080002;
     }
     public static final class layout {
         public static final int activity_confirm=0x7f030000;
--- aProfile/src/com/LTH/aprofile/Confirm.java
+++ aProfile/src/com/LTH/aprofile/Confirm.java
@@ -1,22 +1,116 @@
 package com.LTH.aprofile;
 
+import com.LTH.aprofile.R;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Confirm extends Activity {
+public class Confirm extends Activity implements SensorEventListener {
+
+	private float mLastX, mLastY, mLastZ;
+	private SensorManager mgr;
+	private Sensor acc;
+	private boolean mInitialized;
+	private final float NOISE = (float) 1.0;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_confirm);
+		setContentView(R.layout.activity_main);
+		mInitialized = false;
+		mgr = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+		acc = mgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.confirm, menu);
-		return true;
+	protected void onResume() {
+		mgr.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
+		super.onResume();
 	}
 
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mgr.unregisterListener(this);
+	}
+
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+	}
+
+	public void onSensorChanged(SensorEvent event) {
+		TextView tvX = (TextView) findViewById(R.id.textview1);
+		TextView tvY = (TextView) findViewById(R.id.textview2);
+		TextView tvZ = (TextView) findViewById(R.id.textview3);
+		ImageView iv = (ImageView) findViewById(R.id.image);
+		float x = event.values[0];
+		float y = event.values[1];
+		float z = event.values[2];
+
+		// set values to 0. No movements
+		if (!mInitialized) {
+			mLastX = x;
+			mLastY = y;
+			mLastZ = z;
+			tvX.setText("0.0");
+			tvY.setText("0.0");
+			tvZ.setText("0.0");
+			mInitialized = true;
+		} else {
+
+			float deltaX = (mLastX - x);
+			float deltaY = (mLastY - y);
+			float deltaZ = (mLastZ - z);
+
+			// Filter out Noise
+			if (Math.abs(deltaX) < NOISE)
+				deltaX = (float) 0.0;
+			if (Math.abs(deltaY) < NOISE)
+				deltaY = (float) 0.0;
+			if (Math.abs(deltaZ) < NOISE)
+				deltaZ = (float) 0.0;
+
+			// Re-set the values
+			mLastX = x;
+			mLastY = y;
+			mLastZ = z;
+
+			tvX.setText(Float.toString(deltaX));
+			tvY.setText(Float.toString(deltaY));
+			tvZ.setText(Float.toString(deltaZ));
+
+			//
+			if (Math.abs(deltaX) > Math.abs(deltaY)) {
+				if (deltaX > 0) {
+					iv.setImageResource(R.drawable.arrow_right);
+					iv.setVisibility(View.VISIBLE);
+				} else {
+					iv.setImageResource(R.drawable.arrow_left);
+					iv.setVisibility(View.VISIBLE);
+				}
+
+			} else if (Math.abs(deltaY) > Math.abs(deltaX)) {
+				if (deltaY > 0) {
+					iv.setImageResource(R.drawable.arrow_up);
+				} else {
+					iv.setImageResource(R.drawable.arrow_down);
+				}
+				iv.setVisibility(View.VISIBLE);
+			} else {
+
+				iv.setVisibility(View.INVISIBLE);
+
+			}
+
+		}
+
+	}
 }
