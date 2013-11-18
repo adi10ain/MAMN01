9feea7d787ad73d528f13be996d1893a68a74e3d
Jimmy Tieu <ada09jti@student.lu.se>
2013-11-18 18:16:42 +0100

    added Confirm

--- aProfile/gen/com/LTH/aprofile/R.java
+++ aProfile/gen/com/LTH/aprofile/R.java
@@ -40,20 +40,23 @@ public final class R {
         public static final int textView2=0x7f08000a;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_newprofile=0x7f030001;
-        public static final int activity_set_brightness=0x7f030002;
+        public static final int activity_confirm=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_newprofile=0x7f030002;
+        public static final int activity_set_brightness=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int newprofile=0x7f070001;
-        public static final int set_brightness=0x7f070002;
-        public static final int settings=0x7f070003;
+        public static final int confirm=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int newprofile=0x7f070002;
+        public static final int set_brightness=0x7f070003;
+        public static final int settings=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_confirm=0x7f050007;
         public static final int title_activity_newprofile=0x7f050003;
         public static final int title_activity_set_brightness=0x7f050004;
         public static final int title_activity_set_brightness2=0x7f050005;
